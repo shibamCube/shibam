515dad3a234b66a88d0f146cd604bffc3084f21d
shibamCube <79200267+shibamCube@users.noreply.github.com>
2021-02-18 18:18:13 +0530

    Update edit_self.php

--- edit_self.php
+++ edit_self.php
@@ -1,38 +1,29 @@
-<?php
 
-
-?>
  <html>
-<head>
-<style>
-body
-{
-background-color: lightblue;
-}
-p
-{
-	font-weight:bold;
-}
-</style>
-</head>
-<body>
-<form action="updated(2).php" method="post">
-	<div class="sh1">
-	<fieldset>
-	<p>update your details</p>
-	<?php
-	session_start();
-	$email=$_SESSION['email'];
-		echo("your email is ".$_SESSION['email']);
-		?>
-		<br><br><br>
-	 <input type="text" placeholder="shibam" name="firstName" >first_name<br><br>
-	 <input type="text" placeholder="chandra" name="lastName" >last_name<br><br>
-	<input type="text" placeholder="address" name="address" >address<br><br>
-	
-	
-	<br><br><br>
-	<input type="submit" name="submit" value="save">
-</body>
+	<head>
+	<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<form action="updated(2).php" method="post">
+				<div class="sh3">
+					<fieldset>
+						<h1 id="edit_self">update your details</h1>
+						<?php
+								session_start();
+								$email=$_SESSION['email'];
+								echo("your email id is ".$_SESSION['email']);
+						?>
+						<br><br><br>
+						<label>first_name</label><br>
+						<input type="text" placeholder="shibam" name="firstName" ><br><br>
+						<label>last_name</label><br>
+						<input type="text" placeholder="chandra" name="lastName" ><br><br>
+							<label>address</label><br>
+						<input type="text" placeholder="address" name="address" ><br><br>
+						<br><br><br>
+						<input type="submit" name="submit" value="save">
+					</fieldset>
+				</div>
+			</form>
+	</body>
 </html>
-
