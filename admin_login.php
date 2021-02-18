a68ed0ebb420af3f381513ed4be57f353e3c582b
shibamCube <79200267+shibamCube@users.noreply.github.com>
2021-02-18 18:11:51 +0530

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -2,28 +2,21 @@
 
 <html>
 <head>
-
-<h1>welcome to admin login page</h1>
-<style>
-	h1
-	{
-		text-align: center;
-	}
-	body{
-  background-color: lightblue;
-  text-align: left;
-	}
-	
-</style>
+	<link rel="stylesheet" href="style.css">
+	<div class="admin_h1"><h1>welcome to admin login page</h1></div>
 </head>
 
 <body>
-<form action="admin_home.php" method="post">
-<div class="form">
-<br>
-<input type="email" name="email" required>email<br><br>
-<input type="password" name="password" required>password<br><br>
-<input type="submit" value="login">
-</form>
+	<!this is the login form of admin>
+	<form action="admin_home.php" method="post">
+		<div class="form_admin">
+			<br>
+			<label>email</label><br><br>
+			<input type="email" name="email" required><br><br><br>
+			<label> password</label><br><br>
+			<input type="password" name="password" required><br><br>
+			<input type="submit" value="login">
+		</div>
+	</form>
 </body>
 </html>
