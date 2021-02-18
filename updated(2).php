e3be0915d24ca3e621d4650d51603c22db2eeaef
shibamCube <79200267+shibamCube@users.noreply.github.com>
2021-02-18 18:18:45 +0530

    Update updated(2).php

--- updated(2).php
+++ updated(2).php
@@ -1,45 +1,42 @@
 <?php
-session_start();
-//echo($email);
-$mysqli = new mysqli("localhost", "root", "12345678", "FORM");
- // $result = $mysqli->query("SELECT * FROM FORM_DETAILS WHERE email='$email';");
- //$row=$result->fetch_assoc();
- $email=$_SESSION['email'];
- $firstName=$_POST['firstName'];
- $lastName=$_POST['lastName'];
- $address=$_POST['address'];
- $password=$_SESSION['password'];
- if(strcmp($firstName,"")==0)
- {
- 	$firstName=$_SESSION['firstName'];
- }
- if(strcmp($lastName,"")==0)
- {
- 	$lastName=$_SESSION['lastName'];
- }
- if(strcmp($address,"")==0)
- {
- 	$address=$_SESSION['address'];
- }
- $result=$mysqli->query("UPDATE FORM_DETAILS SET firstName='$firstName', lastName='$lastName',Address='$address', email='$email' WHERE email ='$email';");
- 
+		session_start();
+		$mysqli = new mysqli("localhost", "root", "12345678", "FORM");
+		if($mysqli->connect_error)
+			{
+				echo("<br/>conncetion error<br/>");
+			}
+		else
+		{
+			 $email=$_SESSION['email'];
+			 $firstName=$_POST['firstName'];
+			  $firstName=filter_var($firstName, FILTER_SANITIZE_STRING);
+			 $lastName=$_POST['lastName'];
+			  $lastName=filter_var($lastName, FILTER_SANITIZE_STRING);
+			 $address=$_POST['address'];
+			 $password=$_SESSION['password'];
+			 if(strcmp($firstName,"")==0)
+			 {
+			 		$firstName=$_SESSION['firstName'];
+			 }
+			 if(strcmp($lastName,"")==0)
+			 {
+			 		$lastName=$_SESSION['lastName'];
+			 }
+			 if(strcmp($address,"")==0)
+			 {
+			 		$address=$_SESSION['address'];
+			 }
+			 
+			 $result=$mysqli->query("UPDATE FORM_DETAILS SET firstName='$firstName', lastName='$lastName',Address='$address', email='$email' WHERE email ='$email';");
+		}
 ?>
+
 <html>
-<head>
-<style>
-body
-	{
-		background-color: lightblue;
-	}
-	p
-	{
-		font-weight:bold;
-		text-align:center;
-	}
-</style>
-</head>
-<body>
-<p>details updated sucessfully<p><br><br>
-press here to go to <a href="view_user.php">user</a> page
-</body>
+	<head>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<h1 id="updated">details updated sucessfully</h1><br><br><br>
+		<p id="updated1">press here to go to <a href="view_user.php">user</a> page</p>
+	</body>
 </html>
