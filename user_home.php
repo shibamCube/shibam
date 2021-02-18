b2f715963bd87b4591bc3a5c9ed6145a338ac9da
shibamCube <79200267+shibamCube@users.noreply.github.com>
2021-02-18 18:17:12 +0530

    Update user_home.php

--- user_home.php
+++ user_home.php
@@ -1,44 +1,26 @@
 
 <?php
-session_start();
-$mysqli = new mysqli("localhost", "root", "12345678", "FORM");
-if($mysqli->connect_error)
-{
-echo("<br/>not connected<br/>");
-}
-
-	$email=$_POST['email'];
-	$password=$_POST['password'];
-	$_SESSION["email"]=$email;
-	$_SESSION["password"]=$password;
-	$result=$mysqli->query("SELECT email FROM FORM_DETAILS WHERE email='$email' AND password='$password';");
-$row=$result->fetch_assoc();
-        if($row == 0)
+	session_start();
+	$mysqli = new mysqli("localhost", "root", "12345678", "FORM");
+	if($mysqli->connect_error)
 	{
-		echo '<script>alert("you are not an user. first register yourself. you will be redirected to registration page")
-		
-			window.location.assign("home.php");
-		</script>'; 
-		 //header('Location:admin_login.php');
+		echo("<br/>conncetion error<br/>");
 	}
 	else
-	header('Location:view_user.php');
-?>
-<html>
-<head>
-<style>
-	body{
-  background-color: lightblue;
-  text-align: left;
-	}
-	h1
 	{
-		text-align:center;
+		$email=$_POST['email'];
+		$password=$_POST['password'];
+		$_SESSION["email"]=$email;
+		$_SESSION["password"]=$password;
+		$result=$mysqli->query("SELECT email FROM FORM_DETAILS WHERE email='$email' AND password='$password';");
+		$row=$result->fetch_assoc();
+		if($row == 0)
+			{
+				echo '<script>alert("you are not an user. first register yourself. you will be redirected to registration page")
+					  window.location.assign("home.php");
+					  </script>'; 
+			}
+		else
+			header('Location:view_user.php');
 	}
-</style>
-<h1>welcome to user page</h1>
-</head>
-<body>
-
-</body>
-</html>
+?>
