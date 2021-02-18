03344de44a8d4bfe0b297b14c848e0882e71fe6c
shibamCube <79200267+shibamCube@users.noreply.github.com>
2021-02-18 18:16:42 +0530

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -1,33 +1,21 @@
 <?php
-
-
-
-
 ?>
 <html>
-<head>
+	<head>
+		<link rel="stylesheet" href="style.css">
+		<h1 id="user_login">welcome to user login page</h1>
+	</head>
 
-<h1>welcome to user login page</h1>
-<style>
-	h1
-	{
-		text-align: center;
-	}
-	body{
-  background-color: lightblue;
-  text-align: left;
-	}
-	
-</style>
-</head>
-
-<body>
-<form action="user_home.php" method="post">
-<div class="form">
-<br>
-<input type="email" name="email" required>email<br><br>
-<input type="password" name="password" required>password<br><br>
-<input type="submit" value="save">
-</form>
-</body>
+	<body>
+		<form action="user_home.php" method="post">
+				<div class="form">
+						<br>
+						<label>email</label><br>
+						<input type="email" name="email" required><br><br>
+						<label>password</label><br>
+						<input type="password" name="password" required><br><br>
+						<input type="submit" value="save">
+				</div>
+		</form>
+	</body>
 </html>
