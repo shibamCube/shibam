a3e0c6f5b85ce6fb5be38873a67256c15f690a75
shibamCube <79200267+shibamCube@users.noreply.github.com>
2021-02-18 18:13:18 +0530

    Update admin_home.php

--- admin_home.php
+++ admin_home.php
@@ -1,38 +1,24 @@
 
 <?php
-$mysqli = new mysqli("localhost", "root", "12345678", "FORM");
-if($mysqli->connect_error)
-{
-echo("<br/>not connected<br/>");
-}
-
-
-	$email=$_POST['email'];
-	$password=$_POST['password'];
-	//echo($email);
-	$result=$mysqli->query("SELECT email FROM ADMIN WHERE email='$email' AND password='$password';");
-$row=$result->fetch_assoc();
-        if($row == 0)
+	$mysqli = new mysqli("localhost", "root", "12345678", "FORM");
+	if($mysqli->connect_error)
 	{
-		echo '<script>alert("you are not an admin\nyou will be redirected to registration page")
-		
-			window.location.assign("home.php");
-		</script>'; 
-		 //header('Location:admin_login.php');
+		echo("<br/>connection failed<br/>");
 	}
 	else
-	header('Location:view_details.php');
-?>
-<html>
-<head>
-<style>
-	body{
-  background-color: lightblue;
-  text-align: left;
+	{
+		$email=$_POST['email'];
+		$password=$_POST['password'];
+		//echo($email);
+		$result=$mysqli->query("SELECT email FROM ADMIN WHERE email='$email' AND password='$password';");
+		$row=$result->fetch_assoc();
+		if($row == 0)
+		{
+			echo '<script>alert("you are not an admin\nyou will be redirected to registration page")
+				  window.location.assign("home.php");
+				  </script>'; 
+		}
+		else
+			header('Location:view_details.php');
 	}
-	</style>
-</head>
-<body>
- <a href="admin_login.php"></a>
-</body>
-</html>
+?>
