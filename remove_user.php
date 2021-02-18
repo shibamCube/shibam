458028a88c0b5373b7fe150ab5f412ef7d30f582
shibamCube <79200267+shibamCube@users.noreply.github.com>
2021-02-18 18:16:15 +0530

    Update remove_user.php

--- remove_user.php
+++ remove_user.php
@@ -1,7 +1,17 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "12345678", "FORM");
-$em=$_GET['em'];
+	$mysqli = new mysqli("localhost", "root", "12345678", "FORM");
+	if($mysqli->connect_error)
+	{
+		echo("<br/>connection failed<br/>");
+	}
+	else
+	{
+		$em=$_GET['em'];
+		$result=$mysqli->query("DELETE FROM FORM_DETAILS WHERE email = '$em';");
+		if($result!=NULL)
+			echo("<script>alert('data deleted');window.location.assign('view_details.php');</script>");
+		else
+			echo("<script>alert('error occured. data not deleted');window.location.assign('view_details.php');</script>");
 
-$result=$mysqli->query("DELETE FROM FORM_DETAILS WHERE email = '$em';");
-echo("<script>alert('data deleted');window.location.assign('view_details.php');</script>");
+	}
 ?>
